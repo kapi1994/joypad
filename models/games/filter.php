75c52a9f00a7d916b85727f322ddcea3d8d11ca5
kapi1994 <nemanjajovicic13@gmail.com>
2023-09-27 18:44:22 +0200

    filter games

--- models/games/filter.php
+++ models/games/filter.php
@@ -1,8 +1,21 @@
 <?php
 header("Content-type:application/json");
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
-    $limit = $_GET['limit'];
-    $text = isset($_GET['text']) ? trim('') : '';
+    $limit = isset($_GET['limit']) ? $_GET['limit'] : 0;
+    $text = isset($_GET['text']) ? trim($_GET['text']) : '';
+    try {
+        require_once '../../config/connection.php';
+        include '../functions.php';
+        echo json_encode([
+            'games' => getAllGames($limit, $text),
+            'pages' => gamePagination($text),
+            'elPerPage' => ADMIN_LIMIT,
+            'countGames' => countGames($text)
+        ]);
+    } catch (PDOException $th) {
+        echo json_encode($th->getMessage());
+        http_response_code(500);
+    }
 } else {
     http_response_code(404);
 }
