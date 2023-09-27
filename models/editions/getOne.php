ce9f0e081734f7decbe0005fbc4c55ef4e6e0859
kapi1994 <nemanjajovicic13@gmail.com>
2023-09-27 20:15:08 +0200

    get one edition

---
+++ models/editions/getOne.php
@@ -0,0 +1,15 @@
+<?php
+header("Content-type:application/json");
+if ($_SERVER['REQUEST_METHOD'] == "GET") {
+    $id = $_GET['id'];
+    try {
+        require_once '../../config/connection.php';
+        include '../functions.php';
+        echo json_encode(getEdition($id));
+    } catch (PDOException $th) {
+        echo json_encode($th->getMessage());
+        http_response_code(500);
+    }
+} else {
+    http_response_code(404);
+}
