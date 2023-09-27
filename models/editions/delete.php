bf79854562a7fccc443910e9a5fa70db60d6fd0f
Nemanja Jovicic <127789955+kapi1994@users.noreply.github.com>
2023-09-28 01:23:15 +0200

    soft delete functionality

--- models/editions/delete.php
+++ models/editions/delete.php
@@ -3,52 +3,21 @@ header("Content-type:application/json");
 if($_SERVER["REQUEST_METHOD"] == "POST"){
 
  $id = $_POST["id"];
- $game_id = $_POST["game_id"];
- $platform = $_POST["platform"];
- $price = $_POST["price"];
- $image = isset($_FILES["image"]) ? 
-     $_FILES["image"] ? "";
- $cover = $_POST["cover"];
- 
- include "../validation.php";
- $editionValidation =  editionFormValidation($platform, $price, $image);
+ $status = $_POST["status"];
 
- if(count($editionValidation) > 0){
-  foreach($editionValidation  as $error){
-   echo json_encode($error);
-   http_response_code(422);
-  }
- }
- else{
-   try{
+ try {
    require_once "../../config/connection.php";
    include "../functions.php";
-   
-   $checkGameEdition  = checkGameEdition($game_id, $platform);
-   
-   if($checkGameEdition && $checkGameEdition-> id != $id){
-     echo json_encode("That ge edition allready exusts");
-    http_response_code(409);
-   }else{
-      
-      $new_image_name = "";
-      if($image !=""){
-        $new_image_name = upload_image($image);
-        remove_image($cover);
-      }
 
-      updateEdition($id,$platform,$price,$image);
+   changeStatus("game_edition", $status, $id);
+  echo json_encode(GrtGameEditionFullRow($id));
 
-       echo json_encode(getEditionFullRow($id));
-
-     }
-   } 
-   catch(PDOException $th){
-    echo json_encode($th->getMessage());
-    http_response_code(500);
-   }
  }
+ catch(PDOException $th){
+   echo json_encode($th->getMessage());
+   http_respinse_code(500);
 }
-else{
+
+}else
  http_response_code(404);
 }
