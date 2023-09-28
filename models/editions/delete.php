a2e6018a7c4bbe677ba34c975dba500f021b887d
kapi1994 <nemanjajovicic13@gmail.com>
2023-09-28 07:43:00 +0200

    Replaced syntax errors

--- models/editions/delete.php
+++ models/editions/delete.php
@@ -1,23 +1,19 @@
 <?php
 header("Content-type:application/json");
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
- $id = $_POST["id"];
- $status = $_POST["status"];
+    $id = $_POST["id"];
+    $status = $_POST["status"];
 
- try {
-   require_once "../../config/connection.php";
-   include "../functions.php";
+    try {
+        require_once "../../config/connection.php";
+        include "../functions.php";
 
-   changeStatus("game_edition", $status, $id);
-  echo json_encode(GrtGameEditionFullRow($id));
-
- }
- catch(PDOException $th){
-   echo json_encode($th->getMessage());
-   http_respinse_code(500);
-}
-
-}else
- http_response_code(404);
-}
+        changeStatus("game_edition", $status, $id);
+        echo json_encode(getOneEditionFullRow($id));
+    } catch (PDOException $th) {
+        echo json_encode($th->getMessage());
+        http_response_code(500);
+    }
+} else
+    http_response_code(404);
