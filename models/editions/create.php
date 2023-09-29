1a13ee84b24873102e5e9548dbde4e3fc1ab1995
kapi1994 <nemanjajovicic13@gmail.com>
2023-09-29 16:01:46 +0200

    fixed errors

--- models/editions/create.php
+++ models/editions/create.php
@@ -1,53 +1,55 @@
 <?php
 header("Content-type:application/json");
-if($_SERVER["REQUEST_METHOD"] =="POST"){
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
- $game_id = $_POST["game_id"];
- $platform = $_POST['platform_id'];
- $price = $_POST['price'];
- $image = $_FILES['image'];
+  $game_id = $_POST["game_id"];
+  $platform = $_POST['platform'];
+  $price = $_POST['price'];
+  $image = $_FILES['image'];
 
- include '../validation.php";
- $editionValidation = editionFormValidation(
-  $platform, $price, $image
- );
+  include "../validations.php";
+  $editionValidation = editionFormValidation(
+    $platform,
+    $price,
+    $image
+  );
 
- if(count($editionValidation) > 0){
-   foreach($editionValidation as $error){
-     http_response_code(422);
-     echo json_encode($error);
-   }
- }
- else{ 
-   try{
+  if (count($editionValidation) > 0) {
+    foreach ($editionValidation as $error) {
+      http_response_code(422);
+      echo json_encode($error);
+    }
+  } else {
+    try {
       require_once "../../config/connection.php";
       include "../functions.php";
 
       $image_name = image_upload($image);
       $checkGameEdition = checkGameEdition(
-        $game_id, $platform
+        $game_id,
+        $platform
       );
 
-      if($checkGameEdition){
-       echo json_encode("That edition allready exists");
-       http_response_code(409);
-      }
-      else{
-        insertNewEdition($game_id, $platform,                 
-$price, $image_name);
+      if ($checkGameEdition) {
+        echo json_encode("That edition allready exists");
+        http_response_code(409);
+      } else {
+        insertNewEdition(
+          $game_id,
+          $platform,
+          $price,
+          $image_name
+        );
         echo json_encode([
-          "editions" => getAllEditions($game_id),
-"message" => "New edition has been added"
+          "editions" => getAllGameEditions($game_id),
+          "message" => "New edition has been added"
         ]);
       }
-   }
-   catch(PDOException $th){
-    echo json_encode($th->getMessage());
-    http_response_code(500);
-   }
- }
- 
-}
-else{
- http_response_code(404);
+    } catch (PDOException $th) {
+      echo json_encode($th->getMessage());
+      http_response_code(500);
+    }
+  }
+} else {
+  http_response_code(404);
 }
