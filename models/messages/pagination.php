f06935ed7afb742f27d0e31288def9d7e2bb4617
kapi1994 <nemanjajovicic13@gmail.com>
2023-10-01 22:21:02 +0200

    Messages pagination

---
+++ models/messages/pagination.php
@@ -0,0 +1,20 @@
+<?php
+header("Content-type:application/json");
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $limit = $_GET['limit'];
+    try {
+        require_once '../../config/connection.php';
+        include '../functions.php';
+        echo json_encode([
+            'messages' => getAllMessages($limit),
+            'pages' => messagePagination(),
+            'elPerPage' => ADMIN_LIMIT
+        ]);
+    } catch (PDOException $th) {
+        echo json_encode($th->getMessage());
+        http_response_code(500);
+    }
+} else {
+    http_response_code(404);
+}
