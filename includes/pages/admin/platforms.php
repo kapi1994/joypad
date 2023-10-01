dd6b34a66e7e42b4ab86167a4c5d4b922982476b
kapi1994 <nemanjajovicic13@gmail.com>
2023-10-01 22:57:55 +0200

    Changed protection based on role

--- includes/pages/admin/platforms.php
+++ includes/pages/admin/platforms.php
@@ -1,5 +1,5 @@
 <?php
-if (!isset($_SESSION['user']) || isset($_SESSION['user']) && $_SESSION['user']->role_id != 1) {
+if (!isset($_SESSION['user']) || isset($_SESSION['user']) && $_SESSION['user']->role_id != 2) {
     header("Location: index.php?page=errorrs&code=403");
 }
 $platforms = getAllPlatforms();
