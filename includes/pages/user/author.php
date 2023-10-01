4dea8531a20fa93d7b4856ed90a7b8d1cce1b007
kapi1994 <nemanjajovicic13@gmail.com>
2023-10-01 23:13:06 +0200

    Author page

---
+++ includes/pages/user/author.php
@@ -0,0 +1,15 @@
+<div class="container">
+    <div class="row mt-5">
+        <div class="col-lg-8 mx-auto">
+            <div class="row">
+                <div class="col-lg-4">
+                    <img src="assets/img/author.jpg" alt="Nemanja Jovicic" class="img-fluid">
+                </div>
+                <div class="col-lg-8">
+                    <p class="fs-4 text-justify"><span class="fs-3">Hi</span>, my name is Nemanja Jovicic. I'm passionate video gamer and currently i'm
+                        working as cnc programmer and operator at car industry.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
